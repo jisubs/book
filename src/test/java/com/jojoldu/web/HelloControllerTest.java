79f1b4aa1ac668cbde6d7bfe4d461e99d865089a
song <mkuoik@gmail.com>
2023-04-15 16:33:25 +0900

    머스타치 수정까지 완료

--- src/test/java/com/jojoldu/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/web/HelloControllerTest.java
@@ -9,8 +9,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class) // 스프링부트 테스트와 Junit 사이에 연결자 역할
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
@@ -25,5 +27,20 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name",name)
+                        .param("amount", String.valueOf(amount)))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.name", is(name)))
+                        .andExpect(jsonPath("$.amount", is(amount)));
+
+
     }
 }
