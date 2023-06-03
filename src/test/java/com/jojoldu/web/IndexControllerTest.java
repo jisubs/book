0f9e61291bc8137dd4108661dbc835ace6a8a691
John Kim <johnkim>
2023-06-03 13:11:49 +0900

    ec2 deploy

--- src/test/java/com/jojoldu/web/IndexControllerTest.java
+++ src/test/java/com/jojoldu/web/IndexControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -21,7 +22,7 @@ public class IndexControllerTest {
         //when
         String body = this.restTemplate.getForObject("/", String.class);
 
-        // then
-        assertThat(body).contains("스프링 부트로 시작하는 웹서비스");
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
